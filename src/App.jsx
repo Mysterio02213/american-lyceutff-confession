90e7761fd04eac966a99929e8a881f28e8c70964
Mysterio02213 <mysterionotmail@gmail.com>
2025-06-23 00:09:26 +0500

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
+import Confession from "./Confession";
 import Admin from "./Admin";
 import Reports from "./Reports";
 import Terms from "./Terms";
 import Landing from "./Landing";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import Home from "./Home"; // Import the new Home page
+import Post from "./Post";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} /> {/* Landing page as root */}
-        <Route path="/confess" element={<Home />} /> {/* Send confession */}
+        <Route path="/confess" element={<Confession />} />{" "}
+        {/* Send confession */}
         <Route path="/admin-mysterio-lahorelahore" element={<Admin />} />
         <Route path="/report" element={<Reports />} />
         <Route path="/terms" element={<Terms />} />
+        <Route path="/signup" element={<SignUp />} /> {/* Sign Up route */}
+        <Route path="/login" element={<Login />} /> {/* Login route */}
+        <Route path="/home" element={<Home />} /> {/* Protected Home page */}
+        <Route path="/post" element={<Post />} /> {/* Plus button redirect */}
       </Routes>
     </Router>
   );
